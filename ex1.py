f0361d41e7b71000dd8db2d7392f38b055a595e1
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-17 09:48:53 -0300

    exercicios

--- ex1.py
+++ ex1.py
@@ -8,3 +8,28 @@ RM,NOME,MEDIA do aluno
 • Arquivo “reprovados.CSV” contendo uma listagem dos nomes dos alunos reprovados na 
 disciplina, contendo RM,NOME,MEDIA do aluno. 
 Para o aluno ser aprovado ele deve ter média igual ou superior a 6.0.'''
+
+with open('notas.csv', 'r') as arquivo:
+     
+    aprovados = open('aprovados.csv', 'w')
+    aprovados.write('RM,NOME,MEDIA\n')
+    reprovados = open ('reprovados.csv', 'w')
+    reprovados.write('RM,NOME,MEDIA\n')
+
+    cont_linha = 1 
+    for linha in arquivo: 
+        if cont_linha > 1:
+            lista = linha.split(';')
+            lista_notas = lista[-4:]
+
+            soma = 0 
+            for nota in lista_notas:
+                soma += float(nota)
+            media = soma / 4
+            print(f'notas: {lista_notas}\t Média: {media:.2f}')
+
+            if media >= 6:
+                aprovados.write(f'{lista[0]}, {lista[1]}, {media:.2f}\n')
+            else: 
+                aprovados.write(f'{lista[0]}, {lista[1]}, {media:.2f}\n')
+        cont_linha += 1
